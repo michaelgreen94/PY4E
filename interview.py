b7a09d2abe7154deae017664baef405779774191
Michael Green <michael.green94@hotmail.com>
2019-08-01 11:47:30 -0600

    created new method

--- interview.py
+++ interview.py
@@ -1,3 +1,7 @@
+# You are given two linked-lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit.
+# Add the two numbers and return it as a linked list.
+
+
 class ListNode(object):
     def __init__(self, x):
         self.val = x
@@ -8,16 +12,24 @@ class linked_list:
     def __init__(self):
         self.head = None
 
-    def addNode(self, val):
+    def prefixNode(self, val):
         newNode = ListNode(val)
         newNode.next = self.head
         self.head = newNode
 
+    def appendNode(self, val):
+        currNode = self.head
+        if currNode:
+            while currNode.next != None:
+                currNode = currNode.next
+            currNode.next = ListNode(val)
+        else:
+            self.head = ListNode(val)
 
-class Solution:
+
+class linkedSolution:
     def addTwoNumbers(self, l1, l2, c=0):
-        output = ListNode(0)
-        temp = output
+        output = linked_list()
         while l1 != None and l2 != None:
             if l1 is None:
                 first = 0
@@ -35,25 +47,61 @@ class Solution:
                 c = 0
             l1 = l1.next
             l2 = l2.next
-            hold = ListNode(Sum)
-            temp.next = hold
-            temp = temp.next
-        return output.next
+            output.appendNode(Sum)
+        return output.head
 
 
 l1 = linked_list()
-l1.addNode(2)
-l1.addNode(4)
-l1.addNode(3)
+l1.prefixNode(2)
+l1.prefixNode(4)
+l1.prefixNode(3)
 
 l2 = linked_list()
-l2.addNode(5)
-l2.addNode(6)
-l2.addNode(4)
+l2.prefixNode(5)
+l2.prefixNode(6)
+l2.prefixNode(4)
 
-result = Solution().addTwoNumbers(l1.head, l2.head)
+l3 = linked_list()
+l3.appendNode(13)
+l3.appendNode(21)
+l3.appendNode(1)
+l3.appendNode(3)
+
+result = linkedSolution().addTwoNumbers(l1.head, l2.head)
 while result:
-    print(result.val)
+    print('linkedSolution - ' + str(result.val))
     result = result.next
 
 # finished coding on 07/31/19
+
+
+# Given a string, find the length of the longest substring without repeating characters.
+
+
+# class Solution:
+#     def lengthOfLongestSubstring(self, s):
+#         prev = None
+#         start = None
+#         count = 0
+#         pos = 0
+#         found = 0
+#         longest = 0
+#         while not found:
+#             letter = s[pos]
+#             if not count:
+#                 start = s[pos]
+#             count += 1
+#             pos += 1
+#             if letter != prev:
+#                 prev = letter
+#             if count and start == letter:
+
+#             if count > longest:
+#                 longest = count
+#             else:
+#                 count = 0
+#         return count
+
+
+# print(Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxx'))
+# # 10
