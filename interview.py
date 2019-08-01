48023e6e5ac0122c5f8b2e4d87a357628e33f27b
Michael Green <michael.green94@hotmail.com>
2019-08-01 12:23:16 -0600

    adding notes

--- interview.py
+++ interview.py
@@ -1,22 +1,32 @@
-# You are given two linked-lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit.
-# Add the two numbers and return it as a linked list.
+# You are given two linked-lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain
+#  a single digit. Add the two numbers and return it as a linked list.
 
 
 class ListNode(object):
+    # Inits a new node with the passed in value X as the .val and assigns .next to none
     def __init__(self, x):
         self.val = x
         self.next = None
 
 
 class linked_list:
+    # Inits a empty object with a single attribute .head set to none
     def __init__(self):
         self.head = None
 
+    # This method calls ListNode to create a newNode with the passed in value, then it assigns the stored value of self.head to its newNode.next attr.
+    # Then it assigns the newNode that was just created to self.head. So for the next time its called, the newNode.next of the newNode will
+    # actully be the previous node that was made. Thus creating the linked_list in reverse.
     def prefixNode(self, val):
         newNode = ListNode(val)
         newNode.next = self.head
         self.head = newNode
 
+    # This method first assigns the value of self.head to currNode, then it checks to see if currNode is true or false. If false it calls ListNode to
+    # create a new node with its passed value and assigns it to self.head. The next time this method is called currNode will not be false, it will be
+    # the previous node created, causing it to hit our if condition as true. The while loop will run as long as the currNode.next value isnt None, and
+    # it will then assign currNode.next to the currNode. Finally it wall call ListNode created the next Node and assign its value to currNode.next.
+    # This makes the first node created the head, and every node created after that, will be the next of the previous.
     def appendNode(self, val):
         currNode = self.head
         if currNode:
